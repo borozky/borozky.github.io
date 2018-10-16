8c48c98b2654535b0558d799e628044c504b78d3
Joshua Orozco <borozky@gmail.com>
2018-10-16 20:54:34 +1100

    Updated title and description

--- ghpageapp/nuxt.config.js
+++ ghpageapp/nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'ghpageapp',
+    title: 'Borozky Github Website',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'VueJS project for borozky.github.io' }
+      { hid: 'description', name: 'description', content: 'Borozky Github Page' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
